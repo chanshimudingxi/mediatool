efd9a93e1839e2cc7de8b8fff8de96dfcba1bc06
chanshimudingxi <chanshimudingxi@gmail.com>
2020-11-19 23:33:17 +0800

    s

--- test_flv.cpp
+++ test_flv.cpp
@@ -9,18 +9,18 @@ int main()
     flvparser.parse_file("test.flv");
     cout<<"file_header:"<<endl
         <<"    mark:"<<flvparser.m_flv_header.mark[0]<<flvparser.m_flv_header.mark[1] <<flvparser.m_flv_header.mark[2]<<endl
-        <<"    version:"<<hex<<flvparser.m_flv_header.version<<endl
-        <<"    flag:"<<hex<<flvparser.m_flv_header.flag<<endl;
+        <<"    version:"<<hex<<(uint16_t)flvparser.m_flv_header.version<<endl
+        <<"    flag:"<<hex<<(uint16_t)flvparser.m_flv_header.flag<<endl;
 
     for(int i=0; i<flvparser.m_flv_tagheaders.size(); ++i)
     {
         FlvTagHeader& tag_header = flvparser.m_flv_tagheaders[i];
         cout<<"tag_header:"<<endl
             <<"    len:"<<dec<<tag_header.pre_tag_len<<endl
-            <<"    type:"<<hex<<tag_header.type<<endl
+            <<"    type:"<<hex<<(uint16_t)tag_header.type<<endl
             <<"    content:"<<dec<<tag_header.GetContentSize()<<endl
             <<"    time:"<<dec<<tag_header.GetTimeMs()<<endl
-            <<"    timeext:"<<tag_header.timeext<<endl
+            <<"    timeext:"<<(uint16_t)tag_header.timeext<<endl
             <<"    streamid:"<<dec<<tag_header.GetStreamId()<<endl;
     }
     return 0;
